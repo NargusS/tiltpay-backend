2cfe5b510559b10395657cc26a42d684fdce4dee
Narguss <alexandre.chanelaw@icloud.com>
2025-10-04 03:54:32 +0200

    Enhance account creation validation with confirmation code
    - Updated CreateAccountValidator to include a separate field for code_confirmation.
    - Modified the code validation to specify the confirmation field for better clarity and validation accuracy.

--- app/domains/auth/validators/create_account_validator.ts
+++ app/domains/auth/validators/create_account_validator.ts
@@ -5,6 +5,11 @@ export const CreateAccountValidator = vine.compile(
     phoneNumber: vine.string().trim().minLength(10).maxLength(10),
     fullName: vine.string().trim().minLength(2),
     tagName: vine.string().trim().minLength(3),
-    code: vine.string().minLength(4).maxLength(6).confirmed(),
+    code: vine
+      .string()
+      .minLength(4)
+      .maxLength(6)
+      .confirmed({ confirmationField: 'code_confirmation' }),
+    code_confirmation: vine.string().minLength(4).maxLength(6),
   })
 )
